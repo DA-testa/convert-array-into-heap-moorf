8944c7da44b20a34932658996fb7c332aae9edd1
Bodya <hocaleangu@gmail.com>
2023-03-20 00:15:35 +0200

    'test'

--- build_heap.py
+++ build_heap.py
@@ -1,42 +1,39 @@
-# python3
-
+import os
 
 def build_heap(data):
     swaps = []
-    # TODO: Creat heap and heap sort
-    # try to achieve  O(n) and not O(n2)
-
-
+    for i in range(len(data) // 2, -1, -1):
+        heapify(data, i, swaps)
     return swaps
 
-
+def heapify(data, i, swaps):
+    size = len(data)
+    min_index = i
+    left = 2 * i + 1
+    right = 2 * i + 2
+    if left < size and data[left] < data[min_index]:
+        min_index = left
+    if right < size and data[right] < data[min_index]:
+        min_index = right
+    if min_index != i:
+        swaps.append((i, min_index))
+        data[i], data[min_index] = data[min_index], data[i]
+        heapify(data, min_index, swaps)
+        
 def main():
-    
-    # TODO : add input and corresponding checks
-    # add another input for I or F 
-    # first two tests are from keyboard, third test is from a file
-
-
-    # input from keyboard
-    n = int(input())
-    data = list(map(int, input().split()))
-
-    # checks if lenght of data is the same as the said lenght
-    assert len(data) == n
-
-    # calls function to assess the data 
-    # and give back all swaps
+    input_str = input()
+    if "I" in input_str:
+        n = int(input())
+        data = list(map(int, input().split()))
+        assert len(data) == n
+    elif "F" in input_str:
+        filename = input()
+        with open(os.path.join('./tests/', filename), mode="r") as file:
+            n = int(file.readline())
+            data = list(map(int, file.readline().split()))
     swaps = build_heap(data)
-
-    # TODO: output how many swaps were made, 
-    # this number should be less than 4n (less than 4*len(data))
-
-
-    # output all swaps
     print(len(swaps))
-    for i, j in swaps:
-        print(i, j)
-
-
+    print('\n'.join(f'{i} {j}' for i, j in swaps))
+        
 if __name__ == "__main__":
     main()
